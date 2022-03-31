0dcc342974b3dcc25c5547622f2fc6eb6e75ff0b
rohitchavan8851 <102647638+rohitchavan8851@users.noreply.github.com>
2022-03-31 09:31:52 +0530

    Add files via upload

---
+++ Rohit.py
@@ -0,0 +1,11 @@
+file_name = "F:\python\Rohit.txt"
+f = open(file_name, 'r')
+data = f.read()
+print(data)
+f.close
+
+file_name = "F:\python\omkar.txt"
+f= open(file_name, 'r')
+data = f.read()
+print(data)
+f.close
